480abaf59b7438d7d2bea3aa9177e1a410213c98
tschubring <tschubring@gmail.com>
2018-08-07 14:55:14 -0500

    add db

--- index.js
+++ index.js
@@ -19,4 +19,23 @@ express()
   }
   res.send(result)
 })
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+  .get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
