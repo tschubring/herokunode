a905bc5dcfb4ae136ec5529094065fe10c016fa2
tschubring <tschubring@gmail.com>
2018-08-07 16:51:46 -0500

    connecting

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-
+const { Pool } = require('pg')
+const DBU ='postgres://fdhnjjxenmrtbl:66eb82d538a2f1f0e623657c571b7a6a7a175a234065a4a3fa94e26eb58e96c6@ec2-54-227-241-179.compute-1.amazonaws.com:5432/dfv16qht4jj8kv'
+const pool = new Pool({
+  connectionString: DBU,
+  ssl: true
+})
+      console.log("DB "+ DBU);
+//process.env.DATABASE_URL
 
 
 express()
@@ -15,31 +22,28 @@ express()
   .get('/cool', (req, res) => res.send(cool()))
 
   .get('/times', (req, res) => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  res.send(result)
-})
-
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      result += i + ' '
+    }
+    res.send(result)
+  })
   .get('/db', async (req, res) => {
   try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table')
-    res.render('pages/db', result)
-    client.release()
-  } catch (err) {
-    console.error(err)
-    res.send("Error " + err)
-  }
-})
-const { Pool } = require('pg')
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-})
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      res.render('pages/db', result);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
