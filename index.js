f8e820c093bd8e34680150fee39019e85d461ed3
tschubring <tschubring@gmail.com>
2018-08-08 09:12:29 -0500

    explicit db worked locally, render broken

--- index.js
+++ index.js
@@ -31,9 +31,10 @@ express()
   })
   .get('/db', async (req, res) => {
   try {
-      const client = await pool.connect()
+      const client = await pool.connect();
       const result = await client.query('SELECT * FROM test_table');
-      res.render('pages/db', result);
+      //res.render('pages/db', result.rows);
+      res.send(result)
       client.release();
     } catch (err) {
       console.error(err);
